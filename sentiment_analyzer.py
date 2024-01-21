52d32c40cd11279cd82ce9282fb56563fd35f919
jacobahodge <153308257+jacobahodge@users.noreply.github.com>
2024-01-21 12:21:35 -0500

    Update and rename Sentiment Analyzer.py to sentiment_analyzer.py

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,7 +1,9 @@
 # 1.) Clone the project repository
+git clone https://github.com/ibm-developer-skills-network/zzrjt-practice-project-emb-ai.git # run this in the terminal.
 
 
 # 2.) Create a sentiment analysis application using Watson NLP library
+
 # 3.) Format the output of the application
 # 4.) Package the application
 # 5.) Run Unit tests on your application

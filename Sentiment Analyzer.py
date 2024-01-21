cdac68eca59de4ff7a02ba9d2d2f8ed3d1775c86
jacobahodge <153308257+jacobahodge@users.noreply.github.com>
2024-01-21 12:19:02 -0500

    Create Sentiment Analyzer.py

---
+++ Sentiment Analyzer.py
@@ -0,0 +1,10 @@
+# 1.) Clone the project repository
+
+
+# 2.) Create a sentiment analysis application using Watson NLP library
+# 3.) Format the output of the application
+# 4.) Package the application
+# 5.) Run Unit tests on your application
+# 6.) Deploy as web application using Flask
+# 7.) Incorporate Error handling
+# 8.) Run static code analysis
